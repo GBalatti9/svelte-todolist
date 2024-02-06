1f1a5494573a6f8e3b457a67675ffe6974f4b212
GBalatti9 <gas.balatti@gmail.com>
2024-02-06 12:39:56 -0300

    Trying derived stores

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -6,8 +6,30 @@
     let select = '';
     const statusList = [ 'Done', 'In Progress', 'To Start'];
 
+    console.log(tasks);
+
+    const handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    if (name === 'input') {
+        initialTask.update(( prevInitialTask ) => {
+            return {
+                ...prevInitialTask,
+                task: value
+            }
+        })
+    }
+    if (name === 'select') {
+        console.log(name, value);
+        initialTask.update(( prevInitialTask ) => {
+            return {
+                ...prevInitialTask,
+                status: value
+            }
+        })
+    }
+    }
+
     const handleSubmit = () => {
-        console.log(task);
     }
 
 
@@ -17,8 +39,8 @@
     <Card class="mx-auto">
     <h2 class="font-bold mb-3">New task</h2>
     <div class="mb-6">
-        <Input id="success" placeholder={ task } class="mb-4" bind:value={ task }/> 
-        <Select bind:value={ select } >
+        <Input id="success" placeholder={ task } class="mb-4" name="input" on:input={(e) => handleInputChange(e) } bind:value={task}/> 
+            <Select bind:value={select} on:change={(e) => handleInputChange(e)} name='select'>
             { #each statusList as option }
             <option value={ option }> { option } </option>
             {/each }
