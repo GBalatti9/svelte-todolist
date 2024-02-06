ab417c5f0a87c0a88117032e3cef6254087c2dd9
GBalatti9 <gas.balatti@gmail.com>
2024-02-06 11:50:04 -0300

    Grid display

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,13 @@
 <script>
-    import { StaticTable } from "./lib";
+    import { Form, StaticTable } from "./lib";
 
 </script>
 
-<main class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-  <section class="w-4/5 mx-auto mt-5">
+<main class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] md:grid md:grid-cols-3 gap-2">
+  <section class="w-11/12 mx-auto col-span-1">
+    <Form />
+  </section>
+  <section class="w-full col-span-2 mt-5"> 
     <StaticTable />
   </section>
 </main>
--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -1,4 +1,24 @@
-<!-- 
-<div class="mb-6">
-    <Label for="success" color="green" class="block mb-2">Your name</Label>
-    <Input id="success" color="green" placeholder="Success input" />  -->
+<script>
+    import { Button, Card, Input, MultiSelect, Select } from "flowbite-svelte";
+
+    let select = '';
+    const status = [ 'Done', 'In Progress', 'To Start']
+
+
+</script>
+
+<form class="text-center mt-5">
+    <Card class="mx-auto">
+    <h2 class="font-bold mb-3">New task</h2>
+    <div class="mb-6">
+        <Input id="success" placeholder="Buy bread..." class="mb-4"/> 
+        <Select bind:value={select} >
+            { #each status as option }
+                <option value={ option }> { option } </option>
+            {/each }
+        </Select>
+
+    </div>
+    <Button outline color="primary">Add</Button>
+    </Card>
+</form>
