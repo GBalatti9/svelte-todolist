2bd072583106174959a1aecb1fa49ea0eefa51a2
GBalatti9 <gas.balatti@gmail.com>
2024-02-06 11:02:56 -0300

    Adding table component with flowbite

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { StaticTable } from "./lib";
+
+</script>
+
+<main class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+  <section class="w-4/5 mx-auto">
+    <StaticTable />
+  </section>
+</main>
---
+++ src/lib/Form.svelte
@@ -0,0 +1,4 @@
+<!-- 
+<div class="mb-6">
+    <Label for="success" color="green" class="block mb-2">Your name</Label>
+    <Input id="success" color="green" placeholder="Success input" />  -->
