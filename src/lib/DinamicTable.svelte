f10b3b626ff94e2f93106ebf028f5641a06724d0
GBalatti9 <gas.balatti@gmail.com>
2024-02-07 12:55:57 -0300

    Adding task in dinamic table working correctly

--- src/lib/DinamicTable.svelte
+++ src/lib/DinamicTable.svelte
@@ -1,13 +1,23 @@
 <script>
     import { Card } from "flowbite-svelte";
+    import { tasks } from "../stores/store";
 
     let status = ['To Start', 'In Progress', 'Done'];
 
 </script>
 
 <main class="grid grid-flow-col gap-2 text-center">
-    <Card class="min-h-screen drop-shadow-2xl">
+    <Card class="min-h-screen drop-shadow-2xl w-8/12">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Task</h5>
+        { #each $tasks as task}    
+            <div>
+                <Card color="primary" class="my-3">
+                    <h6 class="font-bold text-white-900">
+                        { task.task }
+                    </h6>
+                </Card>
+            </div>
+        {/each }
     </Card>
     { #each status as item }
         <Card class="drop-shadow-xl">
