4aff29f1da2976989767e39eac34221461fcbe1e
GBalatti9 <gas.balatti@gmail.com>
2024-02-08 12:15:33 -0300

    CardComponent modularize

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,3 +3,4 @@ export { default as Container } from './Container.svelte';
 export { default as FindTask } from './FindTask.svelte';
 export { default as DinamicTable } from './DinamicTable.svelte';
 export { default as StaticTable } from './StaticTable.svelte';
+export { default as CardComponentDinamic } from './CardComponentDinamic.svelte';
