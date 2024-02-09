19264e4a5906ddcf491426ecc43889115d0dfbb1
GBalatti9 <gas.balatti@gmail.com>
2024-02-09 08:36:18 -0300

    Task list editing working correctly

--- src/lib/TaskList.svelte
+++ src/lib/TaskList.svelte
@@ -12,7 +12,7 @@
     let status = ["To Start", "In Progress", "Done"];
 
     const move = (item, { target }) => {
-        if (target.name === 'button') return;
+        if (target.name === 'button' || target.name === 'Task') return;
         let newTasks = $tasks.map(( task ) => ({
             ...task,
             status: 
@@ -26,6 +26,15 @@
         }))
         tasks.set(newTasks);
         localStorage.setItem('tasks', JSON.stringify($tasks));
+    }
+
+    let input;
+
+    $: if (input) {
+        input.focus();
+    }
+    const handleInput = () => {
+        console.log(input);
     }
 </script>
 
@@ -44,16 +53,30 @@
                         class    = "my-3 cursor-pointer"
                         on:click = {( e ) => move( task, e )}
                     >
+                    { #if task.editing }
+                        <input 
+                            type="text" 
+                            name="Task" 
+                            class="input-edit" 
+                            color="primary"
+                            bind:this={ input } 
+                            bind:value={ task.task } 
+                            on:input={ handleInput }
+                            in:fade
+                            >
+                    { :else }                    
                         <h6 class="font-bold text-white-900 mb-2">
                             {task.task}
                         </h6>
+                    { /if }
                         <div>
                             { #if task.editing }
                                 { #each status as item }
                                     <button 
-                                        class    = "border border-primary-500 px-2 rounded btn" 
+                                        class    = "border border-primary-500 px-2 rounded btn mt-2" 
                                         name     = "button" 
                                         on:click = {() => onStatus( task.id, item )}
+                                        in:fade
                                         >
                                         { item }
                                     </button>
@@ -84,4 +107,14 @@
         color: white;
         transition: all 0.2s ease-in-out;
     }
+
+    .input-edit{
+        border: none;
+        outline: none;
+        background-color: transparent;
+        font-weight: bold;
+        border-radius: 5px;
+        text-align: center;
+    }
+
 </style>
